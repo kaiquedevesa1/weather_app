4540ffae1d19e2a2b86213a00f0165383247dd2f
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2025-04-16 00:11:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,75 @@
-// 6d7163426dd4da7890d52ddccda985d2
+//Variaveis e seleção de elementos
+const apiKey = "6d7163426dd4da7890d52ddccda985d2";
+const apiCountryURL = "https://flagcdn.com/w320/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+//Funções
+
+const getWeatherData = async (city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  return data;
+};
+
+const showWeatherData = async (city) => {
+  try {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+
+    weatherIconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+
+    countryElement.setAttribute(
+      "src",
+      `${apiCountryURL}${data.sys.country.toLowerCase()}.png`
+    );
+
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide");
+  } catch (err) {
+    weatherContainer.classList.remove("hide");
+    weatherContainer.classList.add("error");
+
+    weatherContainer.innerText =
+      "Não foi possível encontrar o clima de uma cidade com este nome ";
+  }
+};
+
+//Eventos
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value;
+
+    showWeatherData(city);
+  }
+});
